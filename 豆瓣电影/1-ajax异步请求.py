fc8649951bbd08eef4690ad88d2f2449544d3788
1024小神 <15670339118@qq.com>
2019-07-21 15:10:33 +0800

    update

---
+++ 豆瓣电影/1-ajax异步请求.py
@@ -0,0 +1,23 @@
+import requests
+
+
+class DouMovie(object):
+    def __init__(self):
+        self.url = ""
+        self.user_agent = {
+            
+        }
+
+    # 创建url
+
+    # 发送请求
+
+    # 解析数据
+
+    # 保存数据
+
+    # 主函数
+
+
+if __name__ == "__main__":
+    pass
