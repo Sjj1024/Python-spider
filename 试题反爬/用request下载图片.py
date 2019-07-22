d2c4e5731f8743d50f1244e365903faf039395d9
1024小神 <15670339118@qq.com>
2019-07-22 11:13:38 +0800

    update

--- 试题反爬/用request下载图片.py
+++ 试题反爬/用request下载图片.py
@@ -1,8 +1,12 @@
 import requests
 
-
 # 测试图片反爬
-ret = requests.get("https://www.ttbcdn.com/d/file/p/2019-07-21/55cd45c7d858df43b9d32ec926094636.jpg")
+url1 = "https://www.ttbcdn.com/d/file/p/2019-07-21/55cd45c7d858df43b9d32ec926094636.jpg"
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+}
+ret = requests.get(url1, headers=header)
+res_jpg = ret.content
 
-with open("2.jpg", "wb") as filr:
-    filr.write(ret.content)
+with open("2.jpg", "wb") as f:
+    f.write(res_jpg)
