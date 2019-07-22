0829fb8103af7698544078856967c5799269fa38
1024小神 <15670339118@qq.com>
2019-07-22 20:19:11 +0800

    update

--- selenium使用/1_test.py
+++ selenium使用/1_test.py
@@ -1,12 +1,18 @@
+import time
 from selenium import webdriver
+
 
 url = "https://movie.douban.com/chart"
 driver = webdriver.Chrome()
 
 driver.get(url)
 
-res1 = driver.find_elements_by_xpath('//div[@class="pl2"]')
-
+time.sleep(2)
+res1 = driver.find_elements_by_xpath('//div[@class="pl2"]/a')
 print(res1)
+print("*"*10)
+for i in res1:
+    # 获取到a标签，并打印a标签的text和链接
+    print(i.text, i.get_attribute("href"))
 
 driver.quit()
