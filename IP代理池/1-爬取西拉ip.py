1cfd5540f7ff8fa9c1982e0976b51207bd3e74ee
1024小神 <15670339118@qq.com>
2019-07-23 14:56:33 +0800

    update

--- IP代理池/1-爬取西拉ip.py
+++ IP代理池/1-爬取西拉ip.py
@@ -1,1 +1,12 @@
-# 待定
+# 待定http://www.xiladaili.com/
+import requests
+
+url = "http://www.xiladaili.com/"
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+}
+
+res = requests.get(url)
+# print(res.content.decode("UTF-8"))
+with open("xila.html", "a+", encoding="UTF-8") as f:
+    f.write(res.content.decode("UTF-8"))
