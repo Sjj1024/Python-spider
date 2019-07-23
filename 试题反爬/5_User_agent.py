09b8ecd1350ac7b63de7eedb52bebc7f7646ee17
1024小神 <15670339118@qq.com>
2019-07-23 16:09:39 +0800

    update

--- 试题反爬/5_User_agent.py
+++ 试题反爬/5_User_agent.py
@@ -1,3 +1,7 @@
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+}
+
 USER_AGENT_LIST = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
     "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
--- 豆瓣电影/1-ajax异步请求.py
+++ 豆瓣电影/1-ajax异步请求.py
@@ -4,19 +4,19 @@ import requests
 class DouMovie(object):
     def __init__(self):
         self.url = ""
-        self.user_agent = {
-            
+        self.user_agent = header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
         }
 
-    # 创建url
+        # 创建url
 
-    # 发送请求
+        # 发送请求
 
-    # 解析数据
+        # 解析数据
 
-    # 保存数据
+        # 保存数据
 
-    # 主函数
+        # 主函数
 
 
 if __name__ == "__main__":
