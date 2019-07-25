6bd53f69592b9e99115f7abcc7875d9809891acb
1024小神 <15670339118@qq.com>
2019-07-25 16:42:44 +0800

    update

--- 试题反爬/用request下载图片.py
+++ 试题反爬/用request下载图片.py
@@ -1,12 +1,12 @@
 import requests
 
 # 测试图片反爬
-url1 = "https://dn-shimo-attachment.qbox.me/ROKdG1YW47AVRSZm/smoke.mp4"
+url1 = "https://www.ttbcdn.com/d/file/p/2019-02-10/40df9ce5d4e5a47a2910cfa43b716aef.jpg"
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
 ret = requests.get(url1, headers=header, verify=False)
 res_jpg = ret.content
 
-with open("smoke.mp4", "wb") as f:
+with open("smoke.jpg", "wb") as f:
     f.write(res_jpg)
