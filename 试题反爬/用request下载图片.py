b8928854894eec5d78f4ca40fc134cb4cd427a0e
1024小神 <15670339118@qq.com>
2019-07-25 16:56:47 +0800

    update

--- 试题反爬/用request下载图片.py
+++ 试题反爬/用request下载图片.py
@@ -1,12 +1,12 @@
 import requests
 
 # 测试图片反爬
-url1 = "https://www.ttbcdn.com/d/file/p/2019-02-10/40df9ce5d4e5a47a2910cfa43b716aef.jpg"
+url1 = "https://wvw.rosmm33.com/pic/upload/2019/07/25/rosikz-No1138-3-1220524767.jpg"
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
 ret = requests.get(url1, headers=header, verify=False)
 res_jpg = ret.content
 
-with open("smoke.jpg", "wb") as f:
+with open("rose.jpg", "wb") as f:
     f.write(res_jpg)
