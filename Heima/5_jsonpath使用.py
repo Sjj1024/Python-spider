6cc5fe4c1d636997c17fb8e11df8e02f4b09d333
1024小神 <15670339118@qq.com>
2019-07-26 19:03:27 +0800

    update

--- Heima/5_jsonpath使用.py
+++ Heima/5_jsonpath使用.py
@@ -1,3 +1,4 @@
+import json
 import re
 import requests
 import jsonpath
@@ -6,10 +7,17 @@ url = 'http://www.lagou.com/lbs/getAllCitySearchLabels.json'
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
-response = requests.get(url, headers=header)
-py_dict = response.json()
-# 只能接收字典，或列表对象
-name_list = jsonpath.jsonpath(py_dict, "$..name")
-# print(name_list)
-with open("1.txt", "w", encoding="utf-8") as f:
-    f.write(response.content.decode("utf-8"))
+# response = requests.get(url, headers=header)
+# py_dict = response.json()
+# # 只能接收字典，或列表对象
+# name_list = jsonpath.jsonpath(py_dict, "$..name")
+# # print(name_list)
+# with open("1.txt", "w", encoding="utf-8") as f:
+#     f.write(response.content.decode("utf-8"))
+
+
+# 从文件中读取到json数据,json.load(文件对象)
+file_obj = open("1.txt", "r", encoding="utf-8")
+json_str = json.load(file_obj)
+print(type(json_str))
+
