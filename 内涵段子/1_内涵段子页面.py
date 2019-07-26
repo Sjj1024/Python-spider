23803d320238499b54cd510785e5c8025e567561
1024小神 <15670339118@qq.com>
2019-07-26 22:50:37 +0800

    update

--- 内涵段子/1_内涵段子页面.py
+++ 内涵段子/1_内涵段子页面.py
@@ -10,8 +10,8 @@ class Duanzi(object):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
         }
 
-    def send_request(self):
-        response = requests.get(self.url1, headers=self.header)
+    def send_request(self, url):
+        response = requests.get(url, headers=self.header)
         html = response.content.decode("gbk")
         # with open("wenda.html", "a+", encoding="utf-8") as f:
         #     f.write(html)
@@ -21,23 +21,36 @@ class Duanzi(object):
     def parse_data(self, html):
         # 第一层解析
         re_obj = re.compile('<h4>.*?<a href=".*?">(.*?)</a>.*?</h4>.*?<div class="f18 mb20">(.*?)</div>', re.S)
-        result = re_obj.findall(html)
-        print(result)
-        return result
+        result1 = re_obj.findall(html)
+        # 第二层解析，换行，标签等
+        second_re_obj = re.compile(r"\s|\r|\n|<(.*?)>|\t|\u3000\u3000|&(.*?);")
+        result2 = []
+        for one in result1:
+            title = second_re_obj.sub("", one[0])
+            content = second_re_obj.sub("", one[1])
+            result2.append((title, content))
+        # print(result2)
+        return result2
 
-    # def save_data(self, result):
-    #     with open("data.txt", "a+", encoding="gbk") as f:
-    #         for i in result:
-    #             str1 = str(i) + "\n"
-    #             f.write(str1)
+    def save_data(self, page, result):
+        # 保存数据到文件中,result是一个元组，包含标题和内容
+        with open("data.html", "a+", encoding="gbk") as f:
+            f.write("----------------------------第{}页数据-------------------------------\n".format(page))
+            for i in result:
+                # i是一个元组title，content
+                str1 = "标题：" + i[0] + "\n"
+                str1 += "内容：" + i[1] + "\n\n"
+                f.write(str1)
 
     def run(self):
-        # 发送请求
-        html = self.send_request()
-        # 解析数据
-        result = self.parse_data(html)
-        # 保存数据
-        # self.save_data(result)
+        for page in range(1, 5):
+            url = "https://www.neihan-8.com/article/list_5_{}.html".format(page)
+            # 发送请求
+            html = self.send_request(url)
+            # 解析数据
+            result = self.parse_data(html)
+            # 保存数据
+            self.save_data(page, result)
 
 
 if __name__ == '__main__':
--- 内涵段子/test.py
+++
@@ -1,6 +0,0 @@
-import re
-
-str1 = '<h4><a href="/article/45117.html"><b>原来月老和孟婆曾是一对</b></a></h4>'
-re_obj = re.compile('<a href=".*">(.*)</a>')
-result = re_obj.findall(str1, re.S)
-print(result)
