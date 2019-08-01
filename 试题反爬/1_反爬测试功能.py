80f0b1b571f0b9d14a4d41c73bc7a85043b4974d
1024小神 <15670339118@qq.com>
2019-08-01 21:32:30 +0800

    update

---
+++ 试题反爬/1_反爬测试功能.py
@@ -0,0 +1,23 @@
+import requests
+from selenium import webdriver
+
+"""
+输入要爬取的网址：
+输入想要的到的元素：
+给出采用的反扒手段：
+
+输入网址：
+
+发送请求：(使用多种方式请求，得到结果）
+1.普通请求，不带user-agent
+2.带上user-agent
+3.带上cookie
+4.带上refere
+5.使用selenium获取elements元素
+6.使用移动端代理获取
+7.
+
+判断需要而内容是否在结果中
+
+
+"""
