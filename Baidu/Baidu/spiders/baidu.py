49d581c93478d4969c44ef669f1428e5f8343f60
1024小神 <15670339118@qq.com>
2019-08-02 14:52:34 +0800

    update

---
+++ Baidu/Baidu/spiders/baidu.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BaiduSpider(scrapy.Spider):
+    name = 'baidu'
+    allowed_domains = ['baidu.com']
+    start_urls = ['http://baidu.com/']
+
+    def parse(self, response):
+        pass
