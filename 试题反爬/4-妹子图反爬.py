a867c3b7a12cd4529397c62642babf5fa926a1c0
1024小神 <15670339118@qq.com>
2019-08-04 17:23:08 +0800

    update

--- 试题反爬/4-妹子图反爬.py
+++ 试题反爬/4-妹子图反爬.py
@@ -1,10 +1,10 @@
 import requests
 
 # 测试图片反爬,妹子图竟然是带上了referer反爬
-url1 = "https://i.meizitu.net/2019/06/13e01.jpg"
+url1 = "https://i.meizitu.net/2019/08/01d01.jpg"
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
-    "Referer": "https://www.mzitu.com/189637/1"
+    "Referer": "https://www.mzitu.com/197306/1"
 }
 ret = requests.get(url1, headers=header, verify=False)
 res_jpg = ret.content
