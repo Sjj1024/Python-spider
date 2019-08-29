dbe567e6f73ba1933fcea5e419692a1e5787b40e
1024小神 <15670339118@qq.com>
2019-08-29 22:18:44 +0800

    update

--- Jdphone/Jdphone/spiders/jdphone.py
+++ Jdphone/Jdphone/spiders/jdphone.py
@@ -4,8 +4,10 @@ import scrapy
 
 class JdphoneSpider(scrapy.Spider):
     name = 'jdphone'
-    allowed_domains = ['jd.com']
-    start_urls = ['http://jd.com/']
+    allowed_domains = ['jd.com', 'search.jd.com']
+    start_urls = ['https://search.jd.com/Search?keyword=%E6%89%8B%E6%9C%BA&enc=utf-8&page=1']
 
     def parse(self, response):
-        pass
+        result = response.xpath('//*[@id="J_goodsList"]/ul/li/div/div[4]/a/@href').getall()
+        for i in result:
+            print(i)
