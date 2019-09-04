aa2fcde61c0c3918fdf8876f1546536ef87ed1e7
1024小神 <15670339118@qq.com>
2019-09-04 14:32:26 +0800

    update

---
+++ 试题反爬/9-操作doc文档.py
@@ -0,0 +1,19 @@
+from docx import *
+from docx.shared import Inches
+
+# 创建一个doc文档
+# file = Document()
+# 创建一个已经存在的文档
+file = Document("test.docx")
+
+# 添加一个标题，等级为2,也就是2级标题
+file.add_heading("段落标题", level=2)
+
+# 添加段落内容
+para = file.add_paragraph("我好喜欢你")
+
+# 添加图片，图片是当前文件夹下的 img.png 图片
+file.add_picture('06b01.jpg',width=Inches(4.0))
+
+# 保存文件
+file.save("test.docx")
