28a699ef136780da0bedf1fadfc97a248658d1b3
1024小神 <15670339118@qq.com>
2019-09-04 14:09:48 +0800

    update

--- 试题反爬/test.py
+++ 试题反爬/test.py
@@ -1,12 +1,17 @@
-from openpyxl import Workbook,load_workbook
+from docx import *
+from docx.shared import Inches
 
-wb = load_workbook("sample.xlsx")
+# 创建一个doc文档
+file = Document()
 
-ws = wb.active
+# 添加一个标题，等级为2,也就是2级标题
+file.add_heading("段落标题", level=2)
 
-for i in ws.rows:
-  for j in i:
-    print(j.value)
+# 添加段落内容
+para = file.add_paragraph("我好喜欢你")
 
+# 添加图片，图片是当前文件夹下的 img.png 图片
+file.add_picture('06b01.jpg',width=Inches(4.0))
 
-
+# 保存文件
+file.save("test.docx")
